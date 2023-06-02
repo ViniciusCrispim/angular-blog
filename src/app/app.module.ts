03fc6c76487ef98f2db940716044e6168d82cefe
Vinícius Crispim <viniciuswoliveira@outlook.com>
2023-06-01 21:34:54 -0300

    Components criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { PrimaryCardComponent } from './components/primary-card/primary-card.component';
+import { SecondaryCardComponent } from './components/secondary-card/secondary-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    PrimaryCardComponent,
+    SecondaryCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
